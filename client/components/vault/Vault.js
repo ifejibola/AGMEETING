e825c81bbcf1190f088890db21215177dd37b5a3
ryankeilty <rkeilty19@gmail.com>
2021-10-29 15:50:36 -0700

    Created vault page

---
+++ client/components/vault/Vault.js
@@ -0,0 +1,195 @@
+import React, {useState} from "react";
+import {
+    Box,
+    Card,
+    Table,
+    TableRow,
+    TableBody,
+    TableCell,
+    Typography,
+    CardHeader,
+    Divider, Button
+} from '@material-ui/core';
+import {Label} from "@material-ui/icons";
+
+const elems = [
+    {
+        id: '4be0679f811115c9d2d28497',
+        agendaItem: "Weekly Report",
+        viewed: "Yes",
+        createdAt: 'Jan 23 2021',
+        name: 'Weekly budget',
+        filetype: 'PDF',
+        status: 'draft',
+        author: 'David'
+    },
+    {
+        id: '4e1cd375bfa59e4347404e20',
+        agendaItem: "New Employees",
+        viewed: "Yes",
+        createdAt: 'Feb 1 2021',
+        name: 'New Prospects Blog',
+        filetype: 'PDF',
+        status: 'active',
+        author: 'Cheryl'
+    },
+    {
+        id: '6b37fdf83195ca7e36622040',
+        agendaItem: "Finances",
+        viewed: "No",
+        createdAt: 'Feb 5 2021',
+        name: 'Fiscal Projections',
+        filetype: 'Spreadsheet',
+        status: 'stopped',
+        author: 'Candace'
+    },
+    {
+        id: 'e3651f8f9565cdbe8d2e5fea',
+        agendaItem: "Marketing plan",
+        viewed: "Yes",
+        createdAt: 'Feb 1 2021',
+        name: 'Best Marketing Course Online',
+        filetype: 'Report',
+        status: 'draft',
+        author: 'Perry'
+    }
+];
+
+const statusOptions = ['In Progress', 'Standing By for Motion', 'Approved', 'Closed'];
+
+const labelColorsMap = {
+    draft: 'secondary',
+    active: 'success',
+    stopped: 'error'
+};
+
+const content = (
+    <Box
+        sx={{
+            backgroundColor: 'background.default',
+            minHeight: '100%',
+            p: 3
+        }}
+    >
+        <Card>
+            <CardHeader title="Vault" />
+            <Divider />
+            <Table>
+                <TableBody>
+                    {elems.map((campaign) => (
+                        <TableRow
+                            key={campaign.id}
+                            sx={{
+                                '&:last-child td': {
+                                    border: 0
+                                }
+                            }}
+                        >
+                            <TableCell>
+                                <Typography
+                                    color="textPrimary"
+                                    sx={{ cursor: 'pointer' }}
+                                    variant="subtitle2"
+                                >
+                                    {campaign.name}
+                                </Typography>
+                                <Box
+                                    sx={{
+                                        alignItems: 'center',
+                                        display: 'flex',
+                                        mt: 1
+                                    }}
+                                >
+                                    <Typography
+                                        color="textSecondary"
+                                        variant="body2"
+                                    >
+                                        {campaign.filetype}
+                                    </Typography>
+                                </Box>
+                            </TableCell>
+                            <TableCell>
+                                <Typography
+                                    color="textPrimary"
+                                    variant="subtitle2"
+                                >
+                                    Author
+                                </Typography>
+                                <Typography
+                                    color="textSecondary"
+                                    sx={{ mt: 1 }}
+                                    variant="body2"
+                                >
+                                    {campaign.author}
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography
+                                    color="textPrimary"
+                                    variant="subtitle2"
+                                >
+                                    Agenda Item
+                                </Typography>
+                                <Typography
+                                    color="textSecondary"
+                                    sx={{ mt: 1 }}
+                                    variant="body2"
+                                >
+                                    {campaign.agendaItem}
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography
+                                    color="textPrimary"
+                                    variant="subtitle2"
+                                >
+
+                                    Viewed?
+                                </Typography>
+                                <Typography
+                                    color="textSecondary"
+                                    sx={{ mt: 1 }}
+                                    variant="body2"
+                                >
+                                    {campaign.viewed}
+                                </Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Button
+                                    color="primary"
+                                    size="small"
+                                    variant="outlined"
+                                >
+                                    View
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Card>
+    </Box>
+);
+
+const Vault = () => {
+        const [status, setStatus] = useState(statusOptions[0]);
+
+        const handleChange = (event) => {
+            setStatus(event.target.value);
+        };
+
+        return (
+            <Box sx={{
+                backgroundColor: 'background.default',
+                minHeight: '100%',
+                p: 3
+            }}>
+                <Card>
+                    {content}
+                </Card>
+            </Box>
+        );
+    }
+;
+
+export default Vault;
