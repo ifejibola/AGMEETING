69845ad75e824a2aa851f526f230b6ec12f3213a
Khai Hung <hungluongkhai@gmail.com>
2022-03-18 22:15:25 -0700

    Remove unnecessary codes.

--- server/routes/redirectLogin.js
+++ server/routes/redirectLogin.js
@@ -10,10 +10,4 @@ export function redirectLogin () {
             // not logged in so redirect to login page with the return url
             return <Navigate to= '/login'  />
         }
-
-        // check if route is restricted by role
-        // if (roles && roles.indexOf(currentUser.role) === -1) {
-        //     // role not authorised so redirect to home page
-        //     return <Redirect to={{ pathname: '/'}} />
-        // }
 }
