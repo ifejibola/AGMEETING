24cf2cbbec64c9a1743876c021b2403740bc8261
ourinsou <ourinsou@gmail.com>
2022-04-01 20:58:42 -0700

    Update agenda.service.js

--- server/services/agenda.service.js
+++ server/services/agenda.service.js
@@ -1,6 +1,7 @@
 export const AgendaService = {
     getAllAgenda,
-    getAgendaById
+    getAgendaById,
+    createAgenda
 };
 
 const headers =  {
@@ -8,4 +9,17 @@ const headers =  {
     'Content-Type': 'application/json',
 }
 
+function getAllAgenda() {
+    const requestOptions = { method: 'GET', headers: headers };
+    return fetch(`/api/v1/agenda`, requestOptions).then(res=>res.json()).catch((error) => console.log(error));
+}
 
+function getAgendaById(id) {
+    const requestOptions = { method: 'GET', headers: headers };
+    return fetch(`/api/v1/agenda/${id}`, requestOptions).then(res=>res.json()).catch((error) => console.log(error));
+}
+
+function createAgenda(data) {
+    const requestOptions = { method: 'POST', headers: headers, body: data };
+    return fetch(`/api/v1/agenda/submit`, requestOptions).catch((error) => console.log(error));
+}
