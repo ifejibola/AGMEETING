e56d6565c6631010a4dd9034b1e97238fd6c6e7b
Karan Gohil <karandilipsinhgohil@gmail.com>
2022-04-28 11:43:18 -0700

    added image of logo

--- client/Component/DashBoard/NavBar.js
+++ client/Component/DashBoard/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link as RouterLink, useNavigate} from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import { experimentalStyled } from "@mui/material";
@@ -7,12 +7,13 @@ import useSettings from "../../hooks/useSettings";
 import MenuIcon from "../../icons/Menu";
 import AccountPopover from "./AccountPopover";
 import NotificationsPopover from "./NotificationsPopover";
-import { Button, ButtonGroup } from '@mui/material';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import { Button, ButtonGroup } from "@mui/material";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 //import {logOut} from "../../login/logout";
-import {authenticationService} from "../../../server/services/authentication.service";
+import { authenticationService } from "../../../server/services/authentication.service";
+import logoAG from "../../../logo/AGMeeting-logo-e1618257409213.png";
 // import MenuIcon from '../../icons/Menu';
 // import ContactsPopover from './ContactsPopover';
 // import ContentSearch from './ContentSearch';
@@ -47,85 +48,71 @@ const NavBar = (props) => {
   const { onSidebarMobileOpen, ...other } = props;
   const { settings } = useSettings();
 
-    const navigate = useNavigate();
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const logOut = () => {
-        authenticationService.logout();
-        navigate("/login");
-    }
+  const logOut = () => {
+    authenticationService.logout();
+    navigate("/login");
+  };
 
-    return (
-        <DashboardNavbarRoot {...other}>
-            <Toolbar sx={{ minHeight: 64 }}>
-                <IconButton
-                    color="inherit"
-                    onClick={onSidebarMobileOpen}
-                    sx={{
-                        display: {
-                            lg: 'none'
-                        }
-                    }}
-                >
-                    <MenuIcon fontSize="small" />
-                </IconButton>
-                <RouterLink to="/">
-                    <h1>
-                        AGMEETING
-                    </h1>
-                    {/* <Logo
-            sx={{
-              display: {
-                lg: 'inline',
-                xs: 'none'
-              },
-              height: 40,
-              width: 40
-            }}
-          /> */}
-                </RouterLink>
-                <Box
-                    sx={{
-                        flexGrow: 1,
-                        ml: 2
-                    }}
-                />
-                {/* <LanguagePopover /> */}
-                <Box sx={{ ml: 1 }}>
-                    {/* <ContentSearch /> */}
-                </Box>
-                <Box sx={{ ml: 1 }}>
-                    {/* <ContactsPopover /> */}
-                </Box>
-                <Box sx={{ ml: 1 }}>
-                    <NotificationsPopover />
-                </Box>
+  return (
+    <DashboardNavbarRoot {...other}>
+      <Toolbar sx={{ minHeight: 64 }}>
+        <IconButton
+          color="inherit"
+          onClick={onSidebarMobileOpen}
+          sx={{
+            display: {
+              lg: "none",
+            },
+          }}
+        >
+          <MenuIcon fontSize="small" />
+        </IconButton>
+        <RouterLink to="/">
+          <img src={logoAG} width="130" height="60"></img>
+        </RouterLink>
+        <Box
+          sx={{
+            flexGrow: 1,
+            ml: 2,
+          }}
+        />
+        {/* <LanguagePopover /> */}
+        <Box sx={{ ml: 1 }}>{/* <ContentSearch /> */}</Box>
+        <Box sx={{ ml: 1 }}>{/* <ContactsPopover /> */}</Box>
+        <Box sx={{ ml: 1 }}>
+          <NotificationsPopover />
+        </Box>
 
-                <Box sx={{ ml: 2 }}>
-                    <AccountPopover />
+        <Box sx={{ ml: 2 }}>
+          <AccountPopover />
+        </Box>
+        <Box sx={{ ml: 1 }}>{/* <AccountPopover /> */}</Box>
 
-                </Box>
-                <Box sx={{ ml: 1 }}>
-                    {/* <AccountPopover /> */}
-                </Box>
-
-                <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                    <Button href='/file'>File</Button>
-                    <Button href='/edit'>Edit</Button>
-                    <Button href='/view'>View</Button>
-                    <Button href='/help'>Help</Button>
-                    <Button onClick={ () => logOut()} href='/login'>Logout</Button>
-                </ButtonGroup>
-            </Toolbar>
-        </DashboardNavbarRoot>
-    );
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Button href="/file">File</Button>
+          <Button href="/edit">Edit</Button>
+          <Button href="/view">View</Button>
+          <Button href="/help">Help</Button>
+          <Button onClick={() => logOut()} href="/login">
+            Logout
+          </Button>
+        </ButtonGroup>
+      </Toolbar>
+    </DashboardNavbarRoot>
+  );
 };
 
 NavBar.propTypes = {
